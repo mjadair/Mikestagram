5784c54dda33548d5b83ed87912e4e46dba5a410
Michael Adair <mjadair91@gmail.com>
2020-07-18 12:07:11 +0100

    Initial Commit

---
+++ Mikestagram/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Mikestagram
+//
+//  Created by Michael Adair on 18/07/2020.
+//  Copyright © 2020 Michael Adair. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
